c5036aa3d49cc1f9635112dd49f44518f82728b6
icyveins7 <icyveins@hotmail.com>
2026-07-16 10:33:27 +0800

    update docstring in histogram example

--- examples/dynamic_image_mask_to_histogram.py
+++ examples/dynamic_image_mask_to_histogram.py
@@ -1,3 +1,20 @@
+"""
+This example shows 2 interactive methods:
+
+1) How to use a rectangular ROI over an image to create a histogram.
+2) How to use a linear region (over a histogram, for visual reference) to create a mask of an image.
+
+The original image gets plotted in subplot 1.
+This is where you can toggle the ROI (with letter 'R' hotkey), which is connected to a
+dynamically updated (grey) histogram in subplot 2.
+
+The histogram (generated once, just for visual reference) is in subplot 2.
+If the ROI is toggled on in subplot 1, you should also see a 2nd, grey histogram in subplot 2;
+this histogram is dynamically generated from the pixels captured in the ROI.
+
+The mask (based on the linear region item in subplot 2) is dynamically updated in subplot 3.
+"""
+
 from pyimgroutines.plots.core import PgFigure, forceShow
 from pyimgroutines.plots.customitems import HistogramItem
 
